e5bfba636d44c89362258063f5c02b104f97bc4e
thisClemons <74882592+thisClemons@users.noreply.github.com>
2021-04-09 13:16:48 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,77 @@
+require_relative '../config/environment.rb'
+
+class Dog
+    
+    attr_accessor :name, :breed, :id
+
+    def initialize(id:nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed        
+    end
+    
+    def self.create_table
+       sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+       SQL
+
+       DB[:conn].execute(sql)        
+    end
+
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE IF EXISTS dogs")        
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES(?, ?)
+        SQL
+        
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+        self
+    end
+
+    def self.create(attributes)
+        new_dog = Dog.new(attributes)
+        new_dog.save        
+    end
+
+    def self.new_from_db(row)
+        new_dog = Dog.new(id:row[0], name:row[1], breed:row[2])  
+    end
+
+    def self.find_by_id(id)
+        sql = <<-SQL
+            SELECT * FROM dogs
+            WHERE id = ?
+        SQL
+
+        dog = DB[:conn].execute(sql, id)
+        Dog.new_from_db(dog[0])
+    end
+
+    def self.find_or_create_by(attributes)
+        sql = <<-SQL
+            SELECT * FROM DOGS 
+            WHERE name = ? AND breed = ?
+        SQL
+        # binding.pry
+        dog = DB[:conn].execute(sql, attributes[:name], attributes[:breed])
+        # binding.pry
+        if !dog.empty?
+            Dog.new_from_db(dog[0])
+        else
+            dog = self.create(attributes)
+        end
+
+    end
+
+end
+
+# binding.pry
